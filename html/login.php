4f5634b5ff001de1c5f3b1ae3e8a117b4eb498f6
LaravelALvin <75327200+LaravelALvin@users.noreply.github.com>
2021-01-24 20:59:03 +0800

    Update login.php

--- html/login.php
+++ html/login.php
@@ -6,21 +6,21 @@ date_default_timezone_set('Asia/Manila');
 <html>
 <head>
     <title>Sign in - My Notes</title>
-    <link rel="icon" href="../img/icon.png">
+    <link rel="icon" href="https://lh3.googleusercontent.com/pw/ACtC-32n3AfVM36ugBczzdtwkxC6Rai1so3ajf9OjGAD0YVgNENFMUt8cR8K8bhnacYMmmDWwZM9jOEVDODzFyZoQoOD_MiBWwIYfUll6EY9-xehPk7gg0be0PXsyCw9RMHX789iqKkTKm0atpBcd6YwdjUBJhpl_qpu=w699-h697-no">
     <link rel="stylesheet" type="text/css" href="../css/app.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <img class="wave" src="../img/wave.png">
+    <img class="wave" src="https://lh3.googleusercontent.com/pw/ACtC-32n3AfVM36ugBczzdtwkxC6Rai1so3ajf9I_zCcnqbP29MGs0eiPZYZujiH_MA7W--ysOMY--eRlDYdeChJeKjaChTcr7ZmvHG8U2n4j7tTcygcrAAq_W39t_SUFShuYKB1F7eSRPS4eOFzK6xZ=w574-h697-no">
     <div class="container">
         <div class="img">
-            <img src="../img/bg.png">
+            <img src="https://lh3.googleusercontent.com/pw/ACtC-32n3AfVM36ugBczzdtwkxC6Rai1so3ajf9ecSpeaRAcwAeP8NbD7ZdRVcaYC0uygeCYq_bzXaMS1eIDC9smsIcvMA8RFkMy0_TYniJ0TJlEtjICiQNSust5Hap3VkcbD9khxAoAEps8s8bRcT=w838-h697-no">
         </div>
         <div class="login-content">
             <form id="loginform" class="form-horizontal" role="form" action="../php/login.php" method="POST">
-                <img src="../img/avatar.png">
+                <img src="https://lh3.googleusercontent.com/pw/ACtC-3coSfX-oFKccmhDZUmYxZOdxToeWwNn9lvcc4WPj-G0C3LkhHYvupyh-DcLctP79SQHqT0_bLRfItDm3YHN3hy3YxJfFNRgfLapYn_FdEnTvg0-vZYbwHPJSdbGpZLLcFXudfo7E3aFIpX1wrcgX0jd=s697-no">
                 <h2 class="title">Welcome</h2>
               <?php
                 if(@$_GET['Empty']==true)
